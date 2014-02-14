a3eeba150f072ae5f6b28d2f23fe953893c4e795
thongta <thongta@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-02-14 04:13:45 +0000

    The first commit
    git-svn-id: https://plugins.svn.wordpress.org/wp-pipes/trunk@857628 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ includes/phputf8/ucfirst.php
@@ -0,0 +1,37 @@
+<?php
+/**
+ * @version    $Id: ucfirst.php 23 2014-01-11 08:24:20Z thongta $
+ * @package    utf8
+ * @subpackage strings
+ */
+
+//---------------------------------------------------------------
+/**
+ * UTF-8 aware alternative to ucfirst
+ * Make a string's first character uppercase
+ * Note: requires utf8_strtoupper
+ *
+ * @param string
+ *
+ * @return string with first character as upper case (if applicable)
+ * @see        http://www.php.net/ucfirst
+ * @see        utf8_strtoupper
+ * @package    utf8
+ * @subpackage strings
+ */
+function utf8_ucfirst( $str ) {
+	switch ( utf8_strlen( $str ) ) {
+		case 0:
+			return '';
+			break;
+		case 1:
+			return utf8_strtoupper( $str );
+			break;
+		default:
+			preg_match( '/^(.{1})(.*)$/us', $str, $matches );
+
+			return utf8_strtoupper( $matches[1] ) . $matches[2];
+			break;
+	}
+}
+
